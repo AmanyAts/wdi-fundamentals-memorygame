60c70b07e0936e3f04bc4ca6bf5699ee91f3187c
amany alattas <amanyalattass@gmail.com>
2019-04-13 21:28:09 +0300

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,22 +1,26 @@
-console.log("Up and running!");
-// var cardOne = "queen";
-// var cardTwo = "queen";
-// var cardThree = "king";
-// var cardFour = "king";
-// console.log("User flipped " + cardOne);
-// console.log("User flipped " + cardThree);
+//console.log("Up and running!");
+
 var cards=["queen" , "queen" ,"King" , "king"];
 var cardsInPlay=[];
-var cardOne = cards[0];
-cardsInPlay.push(cardOne);
-console.log("User flipped queen");
-var cardTwo = cards[2];
-cardsInPlay.push(cardTwo);
-console.log("User flipped king");
-if(cardsInPlay.length ===2){
-	if(cardsInPlay[0]===cardsInPlay[1]){
-        alert("You found a match!");
-	}else{
-		alert("Sorry, try again.");
-	} 
+//checkForMatch
+var checkForMatch = function(){
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+  console.log("You found a match!");
+} else {
+  console.log("Sorry, try again.");
 }
+}//endOfCheckmatch
+
+//flipCard
+var flipCard = function(cardId){
+
+console.log("User flipped "+cards[cardId]);
+cardsInPlay.push(cards[cardId]);
+
+	
+
+}//endOfflipCard
+//calling the functions
+flipCard(0);
+flipCard(2);
+checkForMatch();
